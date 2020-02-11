efffe8745bbedccc4a95f45ca7d6063986c4c1df
Matthew Myers <mym3491@yahoo.com.au>
2020-02-12 00:40:09 +1100

    defined commands

--- jk.py
+++ jk.py
@@ -26,5 +26,45 @@ def version():
     jkm.get_version()
 
 
+@cli.command('jobs-list')
+def jobs_list():
+    """Get List of Jobs."""
+    jkm.get_jobs_list()
+
+
+@cli.command('count-all')
+def count_all():
+    """Get Number of Jobs and Builds"""
+    jkm.get_job_build_count()
+
+
+@cli.command('job-config')
+@click.argument('jobname')
+def job_config(jobname):
+    """Get Job Config"""
+    jkm.get_job_config(jobname)
+
+
+@cli.command('job-info')
+@click.argument('jobname')
+def job_info(jobname):
+    """Get Job Info"""
+    jkm.get_job_info(jobname)
+
+
+@cli.command('build-status')
+@click.argument('jobname')
+def build_status(jobname):
+    """Get last Build status"""
+    jkm.lastBuildStatus(jobname)
+
+
+@cli.command('build-result')
+@click.argument('jobname')
+def build_result(jobname):
+    """Get last Build result"""
+    jkm.lastBuildResult(jobname)
+
+
 if __name__ == '__main__':
     cli()
